c920e82544ce3833c005411a53c6a520367df9e3
Irvan Pramana Putra <darknet1255@gmail.com>
2024-10-31 16:33:15 +0700

    refactor: password users schema

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -4,6 +4,6 @@ export const users = pgTable("users", {
   id: serial("id").primaryKey(),
   name: varchar("name", { length: 100 }).notNull(),
   email: varchar("email", { length: 255 }).unique().notNull(),
-  password: varchar({ length: 100 }).notNull(),
+  password: varchar("password", { length: 100 }).notNull(),
   role: varchar("role", { length: 20 }).notNull().default("user"),
 });
