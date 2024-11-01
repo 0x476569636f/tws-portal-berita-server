76719a6b695241ca6d7ed0a6c15779936cc689eb
Irvan Pramana Putra <darknet1255@gmail.com>
2024-11-01 10:09:43 +0700

    chore: update users schema to use role enum with default value

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -1,11 +1,19 @@
-import { pgTable, serial, timestamp, varchar } from "drizzle-orm/pg-core";
+import {
+  pgEnum,
+  pgTable,
+  serial,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+export const roleEnum = pgEnum("role", ["ADMIN", "USER"]);
 
 export const users = pgTable("users", {
   id: serial("id").primaryKey(),
   name: varchar("name", { length: 100 }).notNull(),
   email: varchar("email", { length: 255 }).unique().notNull(),
   password: varchar("password", { length: 100 }).notNull(),
-  role: varchar("role", { length: 20 }).notNull().default("user"),
+  role: roleEnum("role").notNull().default("USER"),
   created_at: timestamp("created_at").defaultNow().notNull(),
 });
 
