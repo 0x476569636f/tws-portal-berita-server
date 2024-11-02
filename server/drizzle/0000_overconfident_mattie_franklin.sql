6ed6f734c1303860f04238a3e6f272a193c5af3b
Irvan Pramana Putra <darknet1255@gmail.com>
2024-11-02 16:48:45 +0700

    chore: remigrate database schema

--- server/drizzle/0000_overconfident_mattie_franklin.sql
+++ server/drizzle/0000_overconfident_mattie_franklin.sql
@@ -1,8 +1,10 @@
+CREATE TYPE "public"."role" AS ENUM('ADMIN', 'USER');--> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "users" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"name" varchar(100) NOT NULL,
 	"email" varchar(255) NOT NULL,
 	"password" varchar(100) NOT NULL,
-	"role" varchar(20) DEFAULT 'user' NOT NULL,
+	"role" "role" DEFAULT 'USER' NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
 	CONSTRAINT "users_email_unique" UNIQUE("email")
 );
